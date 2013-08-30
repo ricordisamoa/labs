9055c4bbfe3986150cc81655e34ede1654fb4914
ricordisamoa <ricordisamoa@live.it>
2013-08-30 07:36:29 +0200

    adding and fixing GitHub "Fork me" ribbons

--- geocontribs/home.php
+++ geocontribs/home.php
@@ -138,6 +138,6 @@ echo chr(10).'});'.chr(10).'</script><div id="map"></div>';
 
 ?>
 
-<a href="https://github.com/ricordisamoa/geocontribs"><img style="position: fixed; top: 0; right: 0; border: 0;" src="//s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+<a href="https://github.com/ricordisamoa/labs/tree/master/geocontribs"><img style="position: fixed; top: 0; right: 0; border: 0;" src="//s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 </body>
 </html>
